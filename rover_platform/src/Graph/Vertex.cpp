b0fbc685dc37e4d087d69a28145d2abee4c1b50a
Gene Foxwell <gene.foxwell@gmail.com>
2018-04-05 16:17:32 -0400

    turtle bot interface

---
+++ rover_platform/src/Graph/Vertex.cpp
@@ -0,0 +1,71 @@
+#ifndef _INCL_VERTEX
+#define _INCL_VERTEX
+
+class Vertex {
+private:
+  float x;
+  float y;
+  int index;
+  int parent; // used for book keeping during search
+  bool explored;
+public:
+  Vertex();
+  Vertex(float x, float y, int index);
+  int getIndex();
+  float getX();
+  float getY();
+  bool inNhd(float x, float y, float epsilon);
+  bool isExplored();
+  void markExplored();
+  void setParent(int parent);
+  int getParent();
+};
+
+Vertex::Vertex() {
+  this->x = 0;
+  this->y = 0;
+  this->index = -1;
+  this->explored = false;
+}
+
+Vertex::Vertex(float x, float y, int index) {
+  this->x = x;
+  this->y = y;
+  this->index = index;
+  this->explored = false;
+}
+
+int Vertex::getIndex() {
+  return this->index;
+}
+
+float Vertex::getX() {
+  return this->x;
+}
+
+float Vertex::getY() {
+  return this->y;
+}
+
+bool Vertex::inNhd(float x, float y, float epsilon) {
+  float distance = sqrt( pow(x - this->x, 2) + pow(y - this->y, 2) );
+  return distance < epsilon;
+}
+
+bool Vertex::isExplored() {
+  return this->explored;
+}
+
+void Vertex::markExplored() {
+  this->explored = true;
+}
+
+void Vertex::setParent(int parent) {
+  this->parent = parent;
+}
+
+int Vertex::getParent() {
+  return this->parent;
+}
+
+#endif
